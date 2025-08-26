53b4bded31125b4d1ffc94df3ed2c37503c306a6
Olha Shumska <olgashumska65@gmail.com>
2025-08-26 08:07:51 +0200

    feat: CrewPosition model for Crew member for better understend which roles arenot filled yet

--- airports_management_system/models.py
+++ airports_management_system/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 
+class CrewPosition(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
 class Crew(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    position = models.ForeignKey(
+        CrewPosition,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
 
     class Meta:
         verbose_name = "crew member"
