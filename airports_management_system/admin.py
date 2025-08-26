8509885208d7ccb4e1070d149907c603c802db25
Olha Shumska <olgashumska65@gmail.com>
2025-08-26 10:39:11 +0200

    add: all exicting models in airports_management_system register in admin.py

--- airports_management_system/admin.py
+++ airports_management_system/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from airports_management_system.models import (
+    Country,
+    City,
+    CrewPosition,
+    Crew,
+    AirplaneType,
+    Airplane,
+    Airport,
+    Route,
+    Flight,
+)
 
-# Register your models here.
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(CrewPosition)
+admin.site.register(Crew)
+admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(Airport)
+admin.site.register(Route)
+admin.site.register(Flight)
