e60869dbc9ff7fa7454a980af9118491c11caeff
Olha Shumska <olgashumska65@gmail.com>
2025-08-26 08:54:49 +0200

    create: model Route with custom validation fields
    in validation checks that source and destination not same and that distance not less or equal zero

--- airports_management_system/models.py
+++ airports_management_system/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -66,3 +67,33 @@ class Airplane(models.Model):
 class Airport(models.Model):
     name = models.CharField(max_length=255)
     closest_big_city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+
+class Route(models.Model):
+    source = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    distance = models.IntegerField()
+
+    def clean(self):
+        if self.source == self.destination:
+            raise ValidationError("Source and destination must be different")
+        elif self.distance == 0:
+            raise ValidationError("Distance cannot be 0")
+
+    def save(
+            self,
+            *args,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None
+    ):
+        self.full_clean()
+        return super(Route, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    def __str__(self):
+        return (f"{self.source.closest_big_city.name} "
+                f"-> "
+                f"{self.destination.closest_big_city.name}")
