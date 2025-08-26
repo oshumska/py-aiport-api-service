cff4432f86a4fcee88df3de33abb9e6d83b17b64
Olha Shumska <olgashumska65@gmail.com>
2025-08-26 07:37:04 +0200

    create: model crew, a model representing the crew members
    model have fields: first_name, last_name and class Meta where specified verbose names

--- airports_management_system/models.py
+++ airports_management_system/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "crew member"
+        verbose_name_plural = "crew"
