1170ae6842f9d07ab285f0ca0dcdf91bc68057bc
Olha Shumska <olgashumska65@gmail.com>
2025-08-26 11:18:23 +0200

    add: related name to field airplane_type in Airplane model

--- airports_management_system/models.py
+++ airports_management_system/models.py
@@ -62,7 +62,11 @@ class Airplane(models.Model):
     name = models.CharField(max_length=255)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
-    airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE)
+    airplane_type = models.ForeignKey(
+        AirplaneType,
+        on_delete=models.CASCADE,
+        related_name="airplanes",
+    )
 
 
 class Airport(models.Model):
