0534733fb54df41e1cb03e1238afc21e7b648793
Olha Shumska <olgashumska65@gmail.com>
2025-08-27 08:03:18 +0200

    create: serializer class for CrewPostion Model and serializer classes for Crew model
    for crew model created 2 classes so it will be easier understand positon of crew member in list of crew

--- airports_management_system/serializers.py
+++ airports_management_system/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 from airports_management_system.models import (
     Country,
     City,
+    CrewPosition,
+    Crew,
 )
 
 
@@ -18,3 +20,19 @@ class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
         fields = ("id", "name", "country")
+
+
+class CrewPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CrewPosition
+        fields = ("id", "name")
+
+
+class CrewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crew
+        fields = ("id", "first_name", "last_name", "position")
+
+
+class CrewListSerializer(CrewSerializer):
+    position = CrewPositionSerializer(many=False, read_only=True)
