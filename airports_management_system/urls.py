09e2a9e6a21bb2301004df7d2be6bc58d87f5493
Olha Shumska <olgashumska65@gmail.com>
2025-08-28 14:55:45 +0200

    create: viewsets for models CrewPosition and Crew register viewsets in router

--- airports_management_system/urls.py
+++ airports_management_system/urls.py
@@ -4,6 +4,8 @@ from rest_framework import routers
 from airports_management_system.views import (
     CountryViewSet,
     CityViewSet,
+    CrewPositionViewSet,
+    CrewViewSet,
 )
 
 app_name = "airports-manager"
@@ -11,6 +13,8 @@ app_name = "airports-manager"
 router = routers.DefaultRouter()
 router.register("countries", CountryViewSet)
 router.register("cities", CityViewSet)
+router.register("crew_positions", CrewPositionViewSet)
+router.register("crews", CrewViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- airports_management_system/views.py
+++ airports_management_system/views.py
@@ -3,11 +3,17 @@ from rest_framework import viewsets, mixins
 from airports_management_system.models import (
     Country,
     City,
+    CrewPosition,
+    Crew,
 )
 from airports_management_system.serializers import (
     CountrySerializer,
     CitySerializer,
+    CrewPositionSerializer,
+    CrewSerializer,
+    CrewListSerializer,
 )
+
 
 class CountryViewSet(
     mixins.ListModelMixin,
@@ -25,3 +31,27 @@ class CityViewSet(
 ):
     queryset = City.objects.select_related("country")
     serializer_class = CitySerializer
+
+
+class CrewPositionViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = CrewPosition.objects.all()
+    serializer_class = CrewPositionSerializer
+
+
+class CrewViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Crew.objects.select_related("position")
+    serializer_class = CrewSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return CrewListSerializer
+
+        return CrewSerializer
