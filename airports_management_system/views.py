4f59529b7e9e4c6f8a283607479b7f661e15f8ad
Olha Shumska <olgashumska65@gmail.com>
2025-08-28 14:33:41 +0200

    create: views for country and city models

--- airports_management_system/views.py
+++ airports_management_system/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from airports_management_system.models import (
+    Country,
+    City,
+)
+from airports_management_system.serializers import (
+    CountrySerializer,
+    CitySerializer,
+)
+
+class CountryViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+class CityViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = City.objects.select_related("country")
+    serializer_class = CitySerializer
