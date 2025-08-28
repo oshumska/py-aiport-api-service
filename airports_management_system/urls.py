92c314b91a0802d8fe7eea9e664579cb1b7490b0
Olha Shumska <olgashumska65@gmail.com>
2025-08-28 14:34:28 +0200

    create: router and register views for Country and City

--- airports_management_system/urls.py
+++ airports_management_system/urls.py
@@ -1,3 +1,17 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from airports_management_system.views import (
+    CountryViewSet,
+    CityViewSet,
+)
+
 app_name = "airports-manager"
 
-urlpatterns = []
+router = routers.DefaultRouter()
+router.register("countries", CountryViewSet)
+router.register("cities", CityViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
