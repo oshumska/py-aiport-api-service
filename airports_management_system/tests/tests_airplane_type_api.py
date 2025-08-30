14c2f3ea784b86b5483f55bb9fc5c7e5931fe629
Olha Shumska <olgashumska65@gmail.com>
2025-08-30 18:56:02 +0200

    test uanauthenticated user for airplane type

---
+++ airports_management_system/tests/tests_airplane_type_api.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
+
+
+AIRPLANE_TYPE_URL = reverse("airports-manager:airplanetype-list")
+
+
+class UnauthenticatedAirplaneTypeApiTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(AIRPLANE_TYPE_URL)
+        self.assertEqual(
+            res.status_code,
+            status.HTTP_401_UNAUTHORIZED
+        )
