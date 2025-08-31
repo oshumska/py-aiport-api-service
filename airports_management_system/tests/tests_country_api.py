5dd731071bcf55a22f06b49456936555162f407c
Olha Shumska <olgashumska65@gmail.com>
2025-08-31 06:42:04 +0200

    add: tests for authenticated user for country

--- airports_management_system/tests/tests_country_api.py
+++ airports_management_system/tests/tests_country_api.py
@@ -1,10 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
+from airports_management_system.models import Country
+from airports_management_system.serializers import CountrySerializer
 
 COUNTRY_URL = reverse("airports-manager:country-list")
+
+def sample_country(**params):
+    defaults = {
+        "name": "Italy",
+    }
+
+    defaults.update(params)
+    return Country.objects.create(**defaults)
 
 
 class UnauthenticatedCountryApiTests(TestCase):
@@ -18,3 +29,39 @@ class UnauthenticatedCountryApiTests(TestCase):
             res.status_code,
             status.HTTP_401_UNAUTHORIZED
         )
+
+
+class AuthenticatedCountryApiTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            username="test",
+            email="test@test.com",
+            password="<PASSWORD>"
+        )
+        self.client.force_authenticate(self.user)
+
+
+    def test_get_country_list(self):
+        """tests that authenticated user have
+               access to list of countries"""
+        sample_country()
+
+        res = self.client.get(COUNTRY_URL)
+
+        countries = Country.objects.all()
+        serializer = CountrySerializer(countries, many=True)
+
+        self.assertEqual(res.data["results"], serializer.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_country_forbidden(self):
+        """tests that not staff can't create countries"""
+        payload = {
+            "name": "France",
+        }
+
+        res = self.client.post(COUNTRY_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
