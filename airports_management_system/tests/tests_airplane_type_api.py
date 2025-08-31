a213e40459c8766e9de0f633e397563cf986f0cf
Olha Shumska <olgashumska65@gmail.com>
2025-08-31 09:39:19 +0200

    add: tests for authenticated user for airplane_type

--- airports_management_system/tests/tests_airplane_type_api.py
+++ airports_management_system/tests/tests_airplane_type_api.py
@@ -1,10 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
+from airports_management_system.models import AirplaneType
+from airports_management_system.serializers import AirplaneTypeSerializer
 
 AIRPLANE_TYPE_URL = reverse("airports-manager:airplanetype-list")
+
+
+def sample_type(**params):
+    defaults = {
+        "name": "Test Type",
+    }
+    defaults.update(params)
+    return AirplaneType.objects.create(**defaults)
 
 
 class UnauthenticatedAirplaneTypeApiTests(TestCase):
@@ -18,3 +29,31 @@ class UnauthenticatedAirplaneTypeApiTests(TestCase):
             res.status_code,
             status.HTTP_401_UNAUTHORIZED
         )
+
+
+class AuthenticatedAirplaneTypeApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            username="test",
+            email="test@test.com",
+            password="<PASSWORD>"
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_get_list_airplane_types(self):
+        sample_type()
+
+        res = self.client.get(AIRPLANE_TYPE_URL)
+        types = AirplaneType.objects.all()
+        serializer = AirplaneTypeSerializer(types, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data["results"], serializer.data)
+
+    def test_create_airplane_type(self):
+        payload = {
+            "name": "Test Type",
+        }
+        res = self.client.post(AIRPLANE_TYPE_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
