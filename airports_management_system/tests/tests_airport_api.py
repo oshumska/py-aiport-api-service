a571544b46fe8b8ba85a83fc8560c17bf6a25bc7
Olha Shumska <olgashumska65@gmail.com>
2025-08-31 13:23:28 +0200

    add tests for authenticated user for airport

--- airports_management_system/tests/tests_airport_api.py
+++ airports_management_system/tests/tests_airport_api.py
@@ -1,8 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
+from airports_management_system.models import (
+    Country,
+    City,
+    Airport,
+)
+from airports_management_system.serializers import AirportListSerializer
 
 AIRPORT_URL = reverse("airports-manager:airport-list")
 
@@ -18,3 +25,79 @@ class UnauthenticatedAirportApiTests(TestCase):
             res.status_code,
             status.HTTP_401_UNAUTHORIZED
         )
+
+
+class AuthenticatedAirportApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            username="test",
+            email="test@test.com",
+            password="<PASSWORD>"
+        )
+        self.client.force_authenticate(self.user)
+        self.country = Country.objects.create(name="Italy")
+        self.country_2 = Country.objects.create(name="France")
+        self.city = City.objects.create(
+            name="Rome",
+            country=self.country
+        )
+        self.city_2 = City.objects.create(
+            name="Paris",
+            country=self.country_2
+        )
+        self.airport = Airport.objects.create(
+            name="Rome airport",
+            closest_big_city=self.city,
+        )
+        self.airport_2 = Airport.objects.create(
+            name="Paris airport",
+            closest_big_city=self.city_2,
+        )
+        self.serializer_1 = AirportListSerializer(self.airport)
+        self.serializer_2 = AirportListSerializer(self.airport_2)
+
+    def test_get_list_of_airports(self):
+        """tests that authenticated user get list of airports"""
+        res = self.client.get(AIRPORT_URL)
+        airports = Airport.objects.all()
+        serializer = AirportListSerializer(airports, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data["results"], serializer.data)
+
+    def test_filter_by_airport_name(self):
+        """tests filtering by part of airport name"""
+        payload = {
+            "name": "Rome"
+        }
+
+        res = self.client.get(AIRPORT_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(self.serializer_1.data, res.data["results"])
+        self.assertNotIn(self.serializer_2.data, res.data["results"])
+
+    def test_filter_by_country(self):
+        """tests filtering by country id"""
+        payload = {
+            "country": self.country.id,
+        }
+
+        res = self.client.get(AIRPORT_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(self.serializer_1.data, res.data["results"])
+        self.assertNotIn(self.serializer_2.data, res.data["results"])
+
+    def test_filter_by_city(self):
+        """tests filtering by city id"""
+        payload = {
+            "city": self.city.id
+        }
+
+        res = self.client.get(AIRPORT_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(self.serializer_1.data, res.data["results"])
+        self.assertNotIn(self.serializer_2.data, res.data["results"])
