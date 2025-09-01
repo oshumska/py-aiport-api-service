8314f208dda54da890ef48152bd7c2d6c0b090a9
Olha Shumska <olgashumska65@gmail.com>
2025-09-01 16:34:39 +0200

    add: tests for admin for flight

--- airports_management_system/tests/tests_flight_api.py
+++ airports_management_system/tests/tests_flight_api.py
@@ -13,7 +13,8 @@ from airports_management_system.models import (
     Route,
     Airplane,
     AirplaneType,
-    Flight
+    Flight,
+    Crew
 )
 from airports_management_system.serializers import FlightDetailSerializer
 
@@ -30,6 +31,15 @@ def sample_airplane(**params):
     }
     defaults.update(params)
     return Airplane.objects.create(**defaults)
+
+
+def sample_crew(**params):
+    defaults = {
+        "first_name": "Tom",
+        "last_name": "Doe",
+    }
+    defaults.update(params)
+    return Crew.objects.create(**defaults)
 
 
 def detail_url(flight_id):
@@ -166,3 +176,273 @@ class AuthenticatedFlightApiTests(TestCase):
         res = self.client.delete(url)
 
         self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminUserFlightTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            username="test",
+            email="test@test.com",
+            password="<PASSWORD>",
+            is_staff=True,
+        )
+        self.client.force_authenticate(self.user)
+
+        self.country_1 = Country.objects.create(name="County 1")
+        self.city_1 = City.objects.create(
+            name="City 1",
+            country=self.country_1
+        )
+        self.airport_1 = Airport.objects.create(
+            name="Airport 1",
+            closest_big_city=self.city_1,
+        )
+        self.airport_2 = Airport.objects.create(
+            name="Airport 2",
+            closest_big_city=self.city_1,
+        )
+        self.route_1 = Route.objects.create(
+            source=self.airport_1,
+            destination=self.airport_2,
+            distance=100,
+        )
+        self.airplane = sample_airplane()
+
+    def test_create_flight_admin(self):
+        payload = {
+            "route": self.route_1.id,
+            "airplane": self.airplane.id,
+            "departure_time": datetime.datetime(
+                2025,
+                10,
+                10,
+                14,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+            "arrival_time": datetime.datetime(
+                2025,
+                10,
+                10,
+                16,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+        }
+        res = self.client.post(FLIGHT_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    def test_create_flight_with_crew_members(self):
+        crew_1 = sample_crew()
+        crew_2 = sample_crew()
+        payload = {
+            "route": self.route_1.id,
+            "airplane": self.airplane.id,
+            "departure_time": datetime.datetime(
+                2025,
+                10,
+                10,
+                14,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+            "arrival_time": datetime.datetime(
+                2025,
+                10,
+                10,
+                16,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+            "crew_members": [crew_1.id, crew_2.id]
+        }
+        res = self.client.post(FLIGHT_URL, payload)
+        flight = Flight.objects.get(id=res.data["id"])
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertIn(crew_1, flight.crew_members.all())
+        self.assertIn(crew_2, flight.crew_members.all())
+
+    def test_create_flight_bad_request(self):
+        payload = {
+            "route": self.route_1.id,
+            "airplane": self.airplane.id,
+            "departure_time": datetime.datetime(
+                2025,
+                10,
+                10,
+                14,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+            "arrival_time": datetime.datetime(
+                2025,
+                10,
+                10,
+                14,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+        }
+        res = self.client.post(FLIGHT_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        payload.update(
+            arrival_time=datetime.datetime(
+                2025,
+                10,
+                10,
+                12,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+        )
+
+        res = self.client.post(FLIGHT_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_flight_admin(self):
+        route_new = Route.objects.create(
+            source=self.airport_1,
+            destination=self.airport_2,
+            distance=100,
+        )
+        flight = Flight.objects.create(
+            route=self.route_1,
+            airplane=self.airplane,
+            departure_time=datetime.datetime(
+                2025,
+                10,
+                10,
+                14,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+            arrival_time=datetime.datetime(
+                2025,
+                10,
+                10,
+                16,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+        )
+        url = detail_url(flight.id)
+        payload = {
+            "route": route_new.id,
+            "airplane": self.airplane.id,
+            "departure_time": flight.departure_time,
+            "arrival_time": flight.arrival_time,
+            "crew_members": []
+        }
+        res = self.client.put(url, payload)
+        flight.refresh_from_db()
+        self.assertEqual(flight.route, route_new)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_partial_update_flight_admin(self):
+        flight = Flight.objects.create(
+            route=self.route_1,
+            airplane=self.airplane,
+            departure_time=datetime.datetime(
+                2025,
+                10,
+                10,
+                14,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+            arrival_time=datetime.datetime(
+                2025,
+                10,
+                10,
+                16,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+        )
+        url = detail_url(flight.id)
+        payload = {
+            "arrival_time": datetime.datetime(
+                2025, 10, 10, 17, 50, tzinfo=datetime.timezone.utc
+            ),
+        }
+        res = self.client.patch(url, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_partial_update_flight_bad_request(self):
+        flight = Flight.objects.create(
+            route=self.route_1,
+            airplane=self.airplane,
+            departure_time=datetime.datetime(
+                2025,
+                10,
+                10,
+                14,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+            arrival_time=datetime.datetime(
+                2025,
+                10,
+                10,
+                16,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+        )
+        url = detail_url(flight.id)
+        payload = [
+            {
+                "departure_time": datetime.datetime(
+                    2025, 10, 10, 16, 50, tzinfo=datetime.timezone.utc
+                ),
+            },
+            {
+                "departure_time": datetime.datetime(
+                    2025, 10, 10, 17, 50, tzinfo=datetime.timezone.utc
+                ),
+            },
+            {
+                "arrival_time": datetime.datetime(
+                    2025, 10, 10, 14, 50, tzinfo=datetime.timezone.utc
+                ),
+            },
+            {
+                "arrival_time": datetime.datetime(
+                    2025, 10, 10, 13, 50, tzinfo=datetime.timezone.utc
+                ),
+            },
+        ]
+        for date in payload:
+            res = self.client.patch(url, date)
+            self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_delete_flight(self):
+        flight = Flight.objects.create(
+            route=self.route_1,
+            airplane=self.airplane,
+            departure_time=datetime.datetime(
+                2025,
+                10,
+                10,
+                14,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+            arrival_time=datetime.datetime(
+                2025,
+                10,
+                10,
+                16,
+                50,
+                tzinfo=datetime.timezone.utc
+            ),
+        )
+        url = detail_url(flight.id)
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
