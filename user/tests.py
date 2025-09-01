88c5e91f7bb537793a6834445388d557cf8e2eb0
Olha Shumska <olgashumska65@gmail.com>
2025-09-01 19:25:09 +0200

    create: tests for unauthenticated user

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,92 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse("user:register")
+TOKEN_URL = reverse("user:token_obtain_pair")
+USER_ME_URL = reverse("user:manage")
+
+
+class PublicUserApiTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_success(self):
+        payload = {
+            "email": "public@email.com",
+            "password": "password"
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    def test_create_user_already_exist(self):
+        payload = {
+            "email": "public@email.com",
+            "password": "password"
+        }
+        get_user_model().objects.create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_user_invalid_email_password(self):
+        payload = {
+            "email": "username",
+            "password": "password"
+        }
+        get_user_model().objects.create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        payload = {
+            "email": "user@email.com",
+            "password": "4444"
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_generate_user_token(self):
+        payload = {
+            "email": "public@email.com",
+            "password": "password"
+        }
+        get_user_model().objects.create_user(**payload)
+
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertIn("access", res.data)
+        self.assertIn("refresh", res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_generate_token_user_not_exist(self):
+        payload = {
+            "email": "public@email.com",
+            "password": "password"
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_generate_token_credentials_missing(self):
+        payload = {
+            "email": "public@email.com",
+            "password": "password"
+        }
+        get_user_model().objects.create_user(**payload)
+
+        no_email = {
+            "password": "password"
+        }
+        no_password = {
+            "email": "public@email.com",
+        }
+        res = self.client.post(TOKEN_URL, no_email)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        res = self.client.post(TOKEN_URL, no_password)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        res = self.client.get(USER_ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
